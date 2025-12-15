42a7ac2dff03ca98d3a7f183a33f22c3eb97430a
nikolamurgo <murgovskinikola@gmail.com>
2025-12-15 23:17:00 +0100

    method to add the edge in the linkability network adjacency list

--- src/LinkabilityNetwork.java
+++ src/LinkabilityNetwork.java
@@ -3,11 +3,21 @@ import java.util.HashMap;
 public class LinkabilityNetwork {
 
     // sender -> (receiver -> weight)
-    public HashMap<String, HashMap<String, Integer>> linkabilityAdjacencyList;
+    public static HashMap<String, HashMap<String, Integer>> linkabilityAdjacencyList;
 
     public LinkabilityNetwork() {
         linkabilityAdjacencyList = new HashMap<>();
     }
 
-    // TODO: add to adjacency list
+
+    public static void addEdge(String sender, String receiver, int weight){
+        // directed weighted graph, with mult edges and no self loops
+        HashMap<String, Integer> receivers = linkabilityAdjacencyList.get(sender);
+        if (receivers == null) {
+            receivers = new HashMap<>();
+            linkabilityAdjacencyList.put(sender, receivers);
+        }
+        receivers.put(receiver, weight);
+
+    }
 }
